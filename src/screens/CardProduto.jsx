adf75448ae572f7bc34a5306eaf9c7695ae9f0a2
Gaturama <bielsobv@gmail.com>
2023-12-14 15:07:41 -0300

    6th commit - adjusting

--- src/screens/CardProduto.jsx
+++ src/screens/CardProduto.jsx
@@ -7,7 +7,7 @@ import imagemCinco from '../assets/img/pneu-continental.jpg';
 import imagemSeis from '../assets/img/pneu-pirelli.jpg';
 import imagemSete from '../assets/img/volante-lotse.png';
 import imagemOito from '../assets/img/volante-lotseMaxx.png';
-import imagemNove from '../assets/img/volante-lotseRing.png';
+import imagemNove from '../assets/img/volante-lotseVelox.png';
 import imagemDez from '../assets/img/filtro-ar.png';
 import imagemOnze from '../assets/img/filtro-intake.png';
 import imagemDoze from '../assets/img/filtro-oleo.png';
@@ -20,20 +20,20 @@ import Card from 'react-bootstrap/Card';
 const produtos = [
     { imagem: imagemUm, title: 'Audi', text: 'Rodas Aro 18 Audi RS6 Avant | Preta Diamantada', value: 'R$ 7.199,99' },
     { imagem: imagemDois, title: 'BMW', text: 'Rodas Aro 19 BMW M4 CS | Preta Diamantada', value: 'R$ 5.599,00' },
-    { imagem: imagemTreze, title: 'Mercedes', text: 'Rodas Aro 19 Mercedes C63 AMG | Preta Diamantada', value: 'R$ 8.049,00'},
-    { imagem: imagemTres, title: 'Volkswagen', text: 'Rodas Aro 20 Vw Tiguan R Line | Preta Diamantada', value: 'R$ 7.999,99'  },
-    { imagem: imagemQuatro, title: 'Bridgestone', text: 'Pneu Bridgestone Aro 18 Potenza S001', value: 'R$ 3.799,90'  },
-    { imagem: imagemCinco, title: 'Continental', text: 'Pneu Continental Aro 18 ContiSportContact 3', value: 'R$ 3.245,90'  },
-    { imagem: imagemQuatorze, title: 'Farroad', text: 'Pneu Farroad Aro 17 X-Arrow 225', value: 'R$ 518,13'},
-    { imagem: imagemSeis, title: 'Pirelli', text: 'Pneu Pirelli Aro 19 Scorpion Zero', value: 'R$ 2.639,90'  },
-    { imagem: imagemSete, title: 'Lotse', text: 'Volante Lotse Dakar', value: 'R$ 745,90'  },
-    { imagem: imagemQuinze, title: 'Lotse', text: 'Volante Lotse R GT', value: 'R$ 769,90'},
-    { imagem: imagemOito, title: 'Lotse', text: 'Volante Lotse Maxx', value: 'R$ 759,90'  },
-    { imagem: imagemNove, title: 'Lotse', text: 'Volante Lotse Ring', value: 'R$ 759,90'  },
-    { imagem: imagemDez, title: 'K&N', text: 'Filtro de Ar Esportivo', value: 'R$ 379,90'  },
-    { imagem: imagemOnze, title: 'K&N', text: 'Filtro Intake Esportivo', value: 'R$ 3.890,90'  },
-    { imagem: imagemDoze, title: 'K&N', text: 'Filtro de Oleo Performance', value: 'R$ 215,90' },
-    { imagem: imagemDezesseis, title: 'K&N', text: 'Filtro de Combustível', value: 'R$ 210,00'}
+    { imagem: imagemTreze, title: 'Mercedes', text: 'Rodas Aro 19 Mercedes C63 AMG | Preta Diamantada', value: 'R$ 8.049,00' },
+    { imagem: imagemTres, title: 'Volkswagen', text: 'Rodas Aro 20 Vw Tiguan R Line | Preta Diamantada', value: 'R$ 7.999,99' },
+    { imagem: imagemQuatro, title: 'Bridgestone', text: 'Pneu Bridgestone Aro 18 Potenza S001', value: 'R$ 3.799,90' },
+    { imagem: imagemCinco, title: 'Continental', text: 'Pneu Continental Aro 18 ContiSportContact 3', value: 'R$ 3.245,90' },
+    { imagem: imagemQuatorze, title: 'Farroad', text: 'Pneu Farroad Aro 17 X-Arrow 225', value: 'R$ 518,13' },
+    { imagem: imagemSeis, title: 'Pirelli', text: 'Pneu Pirelli Aro 19 Scorpion Zero', value: 'R$ 2.639,90' },
+    { imagem: imagemSete, title: 'Lotse', text: 'Volante Lotse Dakar Preto Camursa', value: 'R$ 745,90' },
+    { imagem: imagemQuinze, title: 'Lotse', text: 'Volante Lotse R GT Preto Camursa', value: 'R$ 769,90' },
+    { imagem: imagemOito, title: 'Lotse', text: 'Volante Lotse Maxx Preto Couro', value: 'R$ 759,90' },
+    { imagem: imagemNove, title: 'Lotse', text: 'Volante Lotse Velox Preto Camursa', value: 'R$ 759,90' },
+    { imagem: imagemDez, title: 'K&N', text: 'Filtro de Ar Cônico Esportivo Universal', value: 'R$ 379,90' },
+    { imagem: imagemOnze, title: 'K&N', text: 'Filtro de Ar Esportivo Intake Universal', value: 'R$ 3.890,90' },
+    { imagem: imagemDoze, title: 'K&N', text: 'Filtro de Oleo Performance Smart Fortwo', value: 'R$ 215,90' },
+    { imagem: imagemDezesseis, title: 'K&N', text: 'Filtro de Combustível Flow PF 1000', value: 'R$ 210,00' }
 ];
 
 function Produto() {
@@ -44,7 +44,7 @@ function Produto() {
                     <Card.Img
                         className={stylesCard["card-img"]}
                         variant="top"
-                        src={produto.imagem} 
+                        src={produto.imagem}
                     />
                     <Card.Body>
                         <Card.Title className={stylesCard["title-card"]}>{produto.title}</Card.Title>
