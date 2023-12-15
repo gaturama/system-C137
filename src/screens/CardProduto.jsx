9370c8138dd7178d2f695b9ce26200495f1e563d
Gaturama <bielsobv@gmail.com>
2023-12-15 14:38:51 -0300

    8th commit - add more cards

--- src/screens/CardProduto.jsx
+++ src/screens/CardProduto.jsx
@@ -14,26 +14,34 @@ import imagemDoze from '../assets/img/filtro-oleo.png';
 import imagemTreze from '../assets/img/roda-mercedes.png';
 import imagemQuatorze from '../assets/img/pneu-farroad.jpg';
 import imagemQuinze from '../assets/img/volante-lotseGT.png';
-import imagemDezesseis from '../assets/img/filtro-combustivel.jpg'
+import imagemDezesseis from '../assets/img/filtro-combustivel.jpg';
+import imagemDezessete from '../assets/img/kit-limpezaK&N.png';
+import imagemDezoito from '../assets/img/pneu-michelin.jpg';
+import imagemDezenovo from '../assets/img/roda-volcano.png';
+import imagemVinte from '../assets/img/volante-lotseNoPrep.png';
 import Card from 'react-bootstrap/Card';
 
 const produtos = [
     { imagem: imagemUm, title: 'Audi', text: 'Rodas Aro 18 Audi RS6 Avant | Preta Diamantada', value: 'R$ 7.199,99' },
-    { imagem: imagemDois, title: 'BMW', text: 'Rodas Aro 19 BMW M4 CS | Preta Diamantada', value: 'R$ 5.599,00' },
+    { imagem: imagemDois, title: 'BMW', text: 'Rodas Aro 19 BMW M4 Competition | Preta Diamantada', value: 'R$ 5.599,00' },
     { imagem: imagemTreze, title: 'Mercedes', text: 'Rodas Aro 19 Mercedes C63 AMG | Preta Diamantada', value: 'R$ 8.049,00' },
     { imagem: imagemTres, title: 'Volkswagen', text: 'Rodas Aro 20 Vw Tiguan R Line | Preta Diamantada', value: 'R$ 7.999,99' },
+    { imagem: imagemDezenovo, title: 'TSW', text: 'Roda Aro 18 Volcano Daimler | Preta Diamantada', value: "985,99"},
+    { imagem: imagemDezoito, title: "Michelin", text: 'Pneu Michelin Aro 18 Pilot Sport 3', value: '2.010,95' },
     { imagem: imagemQuatro, title: 'Bridgestone', text: 'Pneu Bridgestone Aro 18 Potenza S001', value: 'R$ 3.799,90' },
-    { imagem: imagemCinco, title: 'Continental', text: 'Pneu Continental Aro 18 ContiSportContact 3', value: 'R$ 3.245,90' },
+    { imagem: imagemCinco, title: 'Continental', text: 'Pneu Continental Aro 18 ContiSportContact', value: 'R$ 3.245,90' },
     { imagem: imagemQuatorze, title: 'Farroad', text: 'Pneu Farroad Aro 17 X-Arrow 225', value: 'R$ 518,13' },
     { imagem: imagemSeis, title: 'Pirelli', text: 'Pneu Pirelli Aro 19 Scorpion Zero', value: 'R$ 2.639,90' },
     { imagem: imagemSete, title: 'Lotse', text: 'Volante Lotse Dakar Preto Camursa', value: 'R$ 745,90' },
     { imagem: imagemQuinze, title: 'Lotse', text: 'Volante Lotse R GT Preto Camursa', value: 'R$ 769,90' },
     { imagem: imagemOito, title: 'Lotse', text: 'Volante Lotse Maxx Preto Couro', value: 'R$ 759,90' },
     { imagem: imagemNove, title: 'Lotse', text: 'Volante Lotse Velox Preto Camursa', value: 'R$ 759,90' },
+    { imagem: imagemVinte, title: 'Lotse', text: 'Volante Lotse No Prep Alumínio', value: 'R$ 820,00' },
     { imagem: imagemDez, title: 'K&N', text: 'Filtro de Ar Cônico Esportivo Universal', value: 'R$ 379,90' },
     { imagem: imagemOnze, title: 'K&N', text: 'Filtro de Ar Esportivo Intake Universal', value: 'R$ 3.890,90' },
-    { imagem: imagemDoze, title: 'K&N', text: 'Filtro de Oleo Performance Smart Fortwo', value: 'R$ 215,90' },
-    { imagem: imagemDezesseis, title: 'K&N', text: 'Filtro de Combustível Flow PF 1000', value: 'R$ 210,00' }
+    { imagem: imagemDoze, title: 'K&N', text: 'Filtro de Oleo Performance Smart', value: 'R$ 215,90' },
+    { imagem: imagemDezesseis, title: 'K&N', text: 'Filtro de Combustível Flow PF 1000', value: 'R$ 210,00' },
+    { imagem: imagemDezessete, title: 'K&N', text: 'Squeeze Kit Limpeza Filtro de Ar', value: 'R$ 149,99' }
 ];
 
 function Produto() {
